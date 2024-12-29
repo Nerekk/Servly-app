40e8c542c20e92ec6f8446c9d7b6d3d71daf45d0
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-12-30 00:10:31 +0100

    Offers page layout implementation

---
+++ app/src/main/java/com/example/servly_app/features/offers/OffersServiceList.kt
@@ -0,0 +1,63 @@
+package com.example.servly_app.features.offers
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.servly_app.R
+import com.example.servly_app.core.theme.AppTheme
+
+@Preview(
+    showBackground = true,
+    locale = "en",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLightEN"
+)
+@Preview(
+    showBackground = true,
+    locale = "pl",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDarkPL"
+)
+@Composable
+fun PreviewServiceList() {
+    AppTheme {
+        OffersServiceList()
+    }
+}
+
+@Composable
+fun OffersServiceList() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(serviceCategories) { category ->
+            OfficeCategoryCard(
+                categoryImage = painterResource(category.categoryImage),
+                categoryName = stringResource(category.categoryName)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/servly_app/features/offers/OffersView.kt
+++ app/src/main/java/com/example/servly_app/features/offers/OffersView.kt
@@ -1,15 +1,53 @@
 package com.example.servly_app.features.offers
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.servly_app.core.theme.AppTheme
+
+@Preview(
+    showBackground = true,
+    locale = "en",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLightEN"
+)
+@Preview(
+    showBackground = true,
+    locale = "pl",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDarkPL"
+)
+@Composable
+fun PreviewMainView() {
+    AppTheme {
+        OffersView()
+    }
+}
 
 @Composable
 fun OffersView() {
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Text(text = "Offers screen")
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column {
+            OffersInfoCard()
+
+            HorizontalDivider(modifier = Modifier.padding(vertical = 16.dp))
+
+            OffersServiceList()
+        }
     }
 }
