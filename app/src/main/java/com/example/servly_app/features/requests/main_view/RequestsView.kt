a4a70ca9953f4a8694fbe8d96075c0572f4bb174
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-03 19:28:35 +0100

    Applying background to RequestView

--- app/src/main/java/com/example/servly_app/features/requests/main_view/RequestsView.kt
+++ app/src/main/java/com/example/servly_app/features/requests/main_view/RequestsView.kt
@@ -1,9 +1,11 @@
-package com.example.servly_app.features.requests
+package com.example.servly_app.features.requests.main_view
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -12,13 +14,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.servly_app.R
 import com.example.servly_app.core.theme.AppTheme
-import com.example.servly_app.features.profile.ProfileView
 
 @Preview(
     showBackground = true,
@@ -47,20 +47,25 @@ fun RequestsView() {
     )
     var selectedTabIndex by remember { mutableStateOf(0) }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        TabRow(selectedTabIndex = selectedTabIndex) {
-            categories.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = { selectedTabIndex = index },
-                    text = { Text(title) }
-                )
+    Box(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.background)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            TabRow(selectedTabIndex = selectedTabIndex) {
+                categories.forEachIndexed { index, title ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text(title) }
+                    )
+                }
             }
-        }
 
-        when (selectedTabIndex) {
-            0 -> RequestList(requests = getInProgressRequests())
-            1 -> RequestList(requests = getFinishedRequests())
+            when (selectedTabIndex) {
+                0 -> RequestList(requests = getInProgressRequests())
+                1 -> RequestList(requests = getFinishedRequests())
+            }
         }
     }
 }
