04a2ab5087b8ffce531e9c5dade755460f4ef960
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-09 18:07:37 +0100

    ArrangedColumn as custom column definition

---
+++ app/src/main/java/com/example/servly_app/features/util/ArrangedColumn.kt
@@ -0,0 +1,22 @@
+package com.example.servly_app.features.util
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ArrangedColumn(
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        content()
+    }
+}
