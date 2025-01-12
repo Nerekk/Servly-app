968bd3fbc6ab0170519429c956915b46360a192c
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-12 15:47:10 +0100

    AuthViewModel injection to auth navhost

--- app/src/main/java/com/example/servly_app/features/authentication/presentation/MainAuthView.kt
+++ app/src/main/java/com/example/servly_app/features/authentication/presentation/MainAuthView.kt
@@ -1,21 +1,23 @@
 package com.example.servly_app.features.authentication.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.servly_app.features.MainView
-import com.example.servly_app.features.authentication.presentation.login.LoginView
-import com.example.servly_app.features.authentication.presentation.login.RoleSelectionView
-import com.example.servly_app.features.authentication.presentation.login.VerificationView
-import com.example.servly_app.features.authentication.presentation.login.WelcomeScreen
+import com.example.servly_app.features.MainAppView
+import com.example.servly_app.features.authentication.presentation.login_view.LoginView
+import com.example.servly_app.features.authentication.presentation.login_view.RoleSelectionView
+import com.example.servly_app.features.authentication.presentation.login_view.VerificationView
+import com.example.servly_app.features.authentication.presentation.login_view.WelcomeScreen
 import com.example.servly_app.features.authentication.presentation.navigation.AuthNavItem
-import com.example.servly_app.features.authentication.presentation.user_data.CustomerFormView
-import com.example.servly_app.features.authentication.presentation.user_data.ProviderFormView
+import com.example.servly_app.features.authentication.presentation.user_data_view.CustomerFormView
+import com.example.servly_app.features.authentication.presentation.user_data_view.ProviderFormView
 
 @Composable
 fun MainAuthView() {
     val navController = rememberNavController()
+    val authViewModel: AuthViewModel = hiltViewModel()
 
     NavHost(
         navController = navController,
@@ -25,10 +27,10 @@ fun MainAuthView() {
             WelcomeScreen(navController)
         }
         composable(AuthNavItem.Login.route) {
-            LoginView(navController)
+            LoginView(navController, authViewModel)
         }
         composable(AuthNavItem.Verification.route) {
-            VerificationView(navController)
+            VerificationView(navController, authViewModel)
         }
         composable(AuthNavItem.RoleSelection.route) {
             RoleSelectionView(navController)
@@ -42,10 +44,10 @@ fun MainAuthView() {
 
 
         composable(AuthNavItem.CustomerMain.route) {
-            MainView()
+            MainAppView()
         }
         composable(AuthNavItem.ProviderMain.route) {
-            MainView()
+            MainAppView()
         }
     }
 }
