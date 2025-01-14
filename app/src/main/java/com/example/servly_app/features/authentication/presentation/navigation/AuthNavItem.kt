51a303832fd32c8a7537fdbaec315b4eba1b7974
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-14 23:11:43 +0100

    Split LoginView into two views

--- app/src/main/java/com/example/servly_app/features/authentication/presentation/navigation/AuthNavItem.kt
+++ app/src/main/java/com/example/servly_app/features/authentication/presentation/navigation/AuthNavItem.kt
@@ -5,6 +5,7 @@ sealed class AuthNavItem(
 ) {
     object Welcome: AuthNavItem("auth_welcome")
     object Login: AuthNavItem("auth_login")
+    object Register: AuthNavItem("auth_register")
     object Verification: AuthNavItem("auth_verification")
     object RoleSelection: AuthNavItem("auth_role")
     object CustomerData: AuthNavItem("auth_customer")
