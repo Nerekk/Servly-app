055ad22107afc960091bd540b8d1d6536ac658de
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-21 15:42:09 +0100

    Repair imports

--- app/src/main/java/com/example/servly_app/features/customer/requests/request_schedule/ScheduleCard.kt
+++ app/src/main/java/com/example/servly_app/features/customer/requests/request_schedule/ScheduleCard.kt
@@ -30,7 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.servly_app.core.ui.theme.AppTheme
-import com.example.servly_app.features.requests.details_view.HeaderSection
+import com.example.servly_app.features.customer.requests.details_view.HeaderSection
 import java.time.LocalDate
 
 @Preview(
