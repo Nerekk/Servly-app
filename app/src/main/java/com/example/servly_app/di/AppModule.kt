04a1e36adb2661f1d56227ea83203ec9200573fd
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-27 22:40:43 +0100

    DI provides update

--- app/src/main/java/com/example/servly_app/di/AppModule.kt
+++ app/src/main/java/com/example/servly_app/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.example.servly_app.di
 
+import com.example.servly_app.core.data.ApiService
+import com.example.servly_app.core.data.RetrofitInstance
+import com.example.servly_app.core.data.RoleDataService
 import com.example.servly_app.features.authentication.data.AuthRepository
 import com.example.servly_app.features.authentication.domain.repository.AuthRepositoryImpl
 import com.example.servly_app.features.authentication.domain.usecase.AuthUseCases
@@ -8,6 +11,15 @@ import com.example.servly_app.features.authentication.domain.usecase.Logout
 import com.example.servly_app.features.authentication.domain.usecase.SignInWithEmail
 import com.example.servly_app.features.authentication.domain.usecase.SignInWithGoogle
 import com.example.servly_app.features.authentication.domain.usecase.SignUpWithEmail
+import com.example.servly_app.core.domain.repository.RoleRepository
+import com.example.servly_app.core.domain.usecase.GetUserRoles
+import com.example.servly_app.features.role_selection.domain.repository.UserFormRepository
+import com.example.servly_app.features.role_selection.domain.usecase.CreateCustomer
+import com.example.servly_app.features.role_selection.domain.usecase.CreateProvider
+import com.example.servly_app.features.role_selection.domain.usecase.CustomerFormUseCases
+import com.example.servly_app.features.role_selection.domain.usecase.ProviderFormUseCases
+import com.example.servly_app.features.role_selection.domain.usecase.UpdateCustomer
+import com.example.servly_app.features.role_selection.domain.usecase.UpdateProvider
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -34,6 +46,57 @@ object AppModule {
             signUpWithEmail = SignUpWithEmail(repository),
             signInWithGoogle = SignInWithGoogle(repository),
             logout = Logout(repository)
+        )
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideApiService() : ApiService {
+        return RetrofitInstance.createService(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoleRepository(apiService: ApiService) : RoleRepository {
+        return RoleRepository(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetUserRoles(repository: RoleRepository) : GetUserRoles {
+        return GetUserRoles(repository)
+    }
+
+
+
+    @Provides
+    @Singleton
+    fun provideRoleDataService() : RoleDataService {
+        return RetrofitInstance.createService(RoleDataService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserFormRepository(roleDataService: RoleDataService) : UserFormRepository {
+        return UserFormRepository(roleDataService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCustomerFormUseCases(userFormRepository: UserFormRepository): CustomerFormUseCases {
+        return CustomerFormUseCases(
+            createCustomer = CreateCustomer(userFormRepository),
+            updateCustomer = UpdateCustomer(userFormRepository)
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideProviderFormUseCases(userFormRepository: UserFormRepository): ProviderFormUseCases {
+        return ProviderFormUseCases(
+            createProvider = CreateProvider(userFormRepository),
+            updateProvider = UpdateProvider(userFormRepository)
         )
     }
 }
