58941fddf97dd531bc36261990f1dd8144e4334d
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-02-04 00:35:49 +0100

    CustomerNavViewModel to handle customer personel data

---
+++ app/src/main/java/com/example/servly_app/features/_customer/CustomerNavViewModel.kt
@@ -0,0 +1,82 @@
+package com.example.servly_app.features._customer
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.servly_app.core.domain.usecase.CustomerUseCases
+import com.example.servly_app.features.role_selection.data.CustomerInfo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class CustomerState(
+    val name: String = "",
+
+    val phoneNumber: String = "",
+
+    val city: String = "",
+
+    val street: String = "",
+
+    val houseNumber: String? = null,
+
+    val latitude: Double? = null,
+
+    val longitude: Double? = null,
+
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+) {
+    fun toCustomerInfo(): CustomerInfo {
+        return CustomerInfo(
+            name,
+            phoneNumber,
+            city,
+            street,
+            houseNumber,
+            latitude,
+            longitude
+        )
+    }
+}
+
+@HiltViewModel
+class CustomerNavViewModel @Inject constructor(
+    private val customerUseCases: CustomerUseCases
+): ViewModel() {
+    private val _customerState = MutableStateFlow(CustomerState())
+    val customerState = _customerState.asStateFlow()
+
+    init {
+        loadCustomer()
+    }
+
+    fun loadCustomer() {
+        viewModelScope.launch {
+            _customerState.update { it.copy(isLoading = true) }
+
+            val result = customerUseCases.getCustomer()
+
+            result.fold(
+                onSuccess = { customer ->
+                    _customerState.update { it.copy(
+                        name = customer.name,
+                        phoneNumber = customer.phoneNumber,
+                        city = customer.city,
+                        street = customer.street,
+                        houseNumber = customer.houseNumber,
+                        latitude = customer.latitude,
+                        longitude = customer.longitude
+                    ) }
+                },
+                onFailure = { e ->
+                    _customerState.update { it.copy(errorMessage = "Error: ${e.message}") }
+                }
+            )
+
+            _customerState.update { it.copy(isLoading = false) }
+        }
+    }
+}
